06e6aaedf8ad1dbd3289848ccc6b9bc8fe4c0cbc
Christian Sutter <christian-sutter@Christians-MBP.fritz.box>
2020-12-23 09:31:22 +0100

    Pickup update & functionality added

---
+++ P4CM4N-VR/Assets/Scripts/floatUpAndDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatUpAndDown : MonoBehaviour
+{
+
+  public float amplitude;
+  public float frequency;
+  public float degreesPerSecond = 15.0f;
+
+  Vector3 posOffset = new Vector3 ();
+  Vector3 tempPos = new Vector3 ();
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    posOffset = transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+    tempPos = posOffset;
+    tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+    transform.position = tempPos;
+  }
+}
---
+++ P4CM4N-VR/Assets/Scripts/scaleUpAndDown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleUpAndDown : MonoBehaviour
+{
+
+  public float amplitude;
+  public Vector3 scale;
+
+  // Update is called once per frame
+  void Update()
+  {
+    for (int i = 0; i < 3; i++){
+      scale[i] = amplitude * Mathf.Sin(Time.time) + 1;
+      transform.localScale = scale;
+    }
+  }
+}
